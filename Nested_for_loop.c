edbdd8573d079754494bd0a5fc44f3d9c1ab2e2b
Awaleh Houssein <awaleh.houssein@g.austincc.edu>
2021-04-26 23:41:42 -0500

    Nested for loop

---
+++ Nested_for_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+
+
+	//Basic
+    int col, row;
+
+    for (row=0;row <3; row++){
+        for(col =0; col<4; col++){
+            printf(" %d", col);
+        }
+        printf("\n");
+    }
+}
