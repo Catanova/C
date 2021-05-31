e439c2a3d8c58d396b257b1b2fb535165ec165bd
Awaleh Houssein <awaleh.houssein@g.austincc.edu>
2021-05-30 23:57:49 -0500

    size

---
+++ 25_size.c
@@ -0,0 +1,12 @@
+
+#include "jump7.h"
+
+
+int main(){
+
+    engineer Sam ={1,"444-22-1000"};
+
+    printf("id: %d\n ssn: %s\n salary %d\n", Sam.id, Sam.ssn, Sam.salary);
+    printf("sizeof Sam is %d\n", sizeof(Sam));
+
+}
