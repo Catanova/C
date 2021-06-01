59493953edabd1e43d9101ae85136662d7355034
Catanova <70447007+Catanova@users.noreply.github.com>
2021-06-01 00:00:18 -0500

    Add files via upload

---
+++ 25_string_lenght.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char x[30];
+  int len;
+
+  printf("Enter a string to calculate its length\n");
+  gets(a);
+
+  len= strlen(x);
+
+  printf("Length of the string = %d\n", len);
+
+  return 0;
+}
