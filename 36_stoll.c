dbd5695a1cf514fcf30b3660353b95ea61958ed7
awalehdek <awaleh.houssein@g.austincc.edu>
2021-08-29 11:46:27 -0500

    difference between atof and atol

--- 36_stoll.c
+++ 36_stoll.c
@@ -5,5 +5,17 @@ int main()
    char xyz[] = "648892";
    long int num = atol (xyz);
    printf("num: %ld \n",  num );
+   char xyz1 [] = "90.57";
+   float num1 = atof(xyz1);
+   //%.2f print flot with two decimail number 
+   printf("num1: %.2f\n", num1);
+   
+   char xyz2 [] = "239.53425378998";
+   double num2 = atof(xyz2);
+
+   //%.11f print float with 11 decimail number 
+   printf("num1: %.11f\n", num2);
+
+
 
 }
